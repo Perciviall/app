8267f992e62a8972c241be8c3a39f403efb8fe1c
Milo Lamas <123337620+Perciviall@users.noreply.github.com>
2024-10-24 14:38:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)  # Hashed password
+
 
 @app.route('/')
 def index():
